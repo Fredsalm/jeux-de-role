bcad1bdcbe91fb405097bc0b9a1d8ae78572353a
fredsalm <salmsimplon@gmail.com>
2018-03-14 10:48:32 +0100

    first commit

--- footer.php
+++
@@ -1,9 +0,0 @@
-	<?php 
-
-	$footer = ' <hr>
-				<footer class="footer-all">
-				Simplon War un jeu proposé par la team #01 de simplon Rodez
-				copyright SA.fredS.
-				</footer>';
-echo $footer;
-?>
