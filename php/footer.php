e1077e01f3b452542d0d5cb6b917410ee0a413f0
fredsalm <salmsimplon@gmail.com>
2018-03-14 10:41:04 +0100

    first commit

---
+++ php/footer.php
@@ -0,0 +1,10 @@
+	<?php 
+
+	$footer = ' 
+				<footer class="footer-all">
+				Simplon War un jeu proposé par la team #01 de simplon Rodez
+				<hr>
+				copyright SA.fredS.
+				</footer>';
+echo $footer;
+?>
---
+++ php/header.php
@@ -0,0 +1,20 @@
+<?php
+
+	$header = '<!DOCTYPE html>
+	<html>
+	<head>
+		<meta charset="utf-8">
+			<link href="css/bootstrap.min.css" rel="stylesheet" >
+			<link rel="stylesheet" type="text/css" href="css/style.css">
+			<link rel="stylesheet" href="css/font-awesome-4.7.0/css/font-awesome.min.css">
+		<title>cv-site</title>
+	</head>
+	<body>
+		<script src="node_modules/jquery/dist/jquery.js"></script>
+		<script type="text/javascript" src="app/app.js"></script>
+	
+	</body>
+	</html>';
+
+echo $header;
+
