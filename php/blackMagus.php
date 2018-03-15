8b31a776d12ecd01cd8c2a53993ad5b60c3f53b7
AntoineCARON <antoinecaron.simplon@gmail.com>
2018-03-15 15:42:34 +0100

    add php

--- php/blackMagus.php
+++ php/blackMagus.php
@@ -4,7 +4,15 @@
         public $_lifePoint = 400;
 
         public function strength(){
-            return $_strength->rand(100, 200);
+            return $_strength -> rand(100, 200);
+        }
+        public function __construct($newstrength, $newlifePoint){
+            $this->$_strength = $newstrength;
+            $this->$_lifePoint = $newlifePoint;
         }
     }
+    
+    $blackMagus = [];
+    $blackMagus [] = new BlackMagus;
+    echo $blackMagus->_strength;
 ?>    
--- php/characters.php
+++ php/characters.php
@@ -1,12 +1,53 @@
 <?php
-    class Characters{
-        public $_blackMagus;
-        public $_blackShadow;
-        public $_dwarf;
-        public $_elf;
-        public $_magician;
-        public $_orc;
-        public $_sauron;
-        public $_warrior;
-    }
+    // class Characters{
+    //     // public $_blackMagus;
+    //     // public $_blackShadow;
+    //     // public $_dwarf;
+    //     // public $_elf;
+    //     // public $_magician;
+    //     // public $_orc;
+    //     // public $_sauron;
+    //     // public $_warrior;
+    //     public $_characters;
+    //     // public $_monsters;
+
+    //     public function fullCharacters(){
+    //         return $this->_characters .' '. '<br/>';
+    //         // return $this->_blackMagus.'<br/>'.$this->_blackShadow.'<br/>'.$this->_dwarf.'<br/>'.$this->_elf.'<br/>'.$this->_magician.'<br/>'.$this->_orc.'<br/>'.$this->_sauron.'<br/>'.$this->_warrior;
+    //     }
+    //     // public function __construct($newblackMagus, $newblackShadow, $newdwarf, $newelf, $newmagician, $neworc, $newsauron, $newwarrior){
+    //     //     $this->_blackMagus = $newblackMagus;
+    //     //     $this->_blackShadow = $newblackShadow;
+    //     //     $this->_dwarf = $newdwarf;
+    //     //     $this->_elf = $newelf;
+    //     //     $this->_magician = $newmagician;
+    //     //     $this->_orc = $neworc;
+    //     //     $this->__sauron = $newsauron;
+    //     //     $this->_warrior = $newwarrior;
+    //     // }
+    //     public function __construct($newCharacters){
+    //         $this->_characters = $newCharacters;
+    //         // $this->_monters = $newmonsters;
+    //     }
+    // }
+
+    // $hero = [];
+    // $hero [] = new Characters('Nain');
+    // $hero [] = new Characters('Elfe');
+    // $hero [] = new Characters('Magicien');    
+    // $hero [] = new Characters('Guerrier');
+
+    // $monster = [];
+    // $monster [] = new Characters('Mage noir');
+    // $monster [] = new Characters('Ombre noir');
+    // $monster [] = new Characters('Orque');
+    // $monster [] = new Characters('Sauron');    
+
+    // foreach ($hero as $heros) {
+    //     echo $heros->fullCharacters();
+    // }
+
+    // foreach ($monster as $monsters) {
+    //     echo $monsters->fullCharacters();
+    // }
 ?>    
